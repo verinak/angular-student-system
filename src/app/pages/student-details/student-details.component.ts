b7108ba7b6edd0ebe33d2bb66e18f4b4d014e178
verinak <verinamichelk@gmail.com>
2026-04-20 00:46:06 +0200

    refactor: use async pipe instead of manual subscribe function

--- src/app/pages/student-details/student-details.component.ts
+++ src/app/pages/student-details/student-details.component.ts
@@ -1,34 +1,28 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { StudentsService } from '../../services/students.service';
 import { Student } from '../../shared/interfaces/student.interface';
-import { Subscription } from 'rxjs';
-import { NgIf } from '@angular/common';
+import { map, Subscription, tap } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { StudentCardComponent } from '../../components/student-card/student-card.component';
 
 @Component({
   selector: 'app-student-details',
   standalone: true,
-  imports: [NgIf, StudentCardComponent],
+  imports: [NgIf, StudentCardComponent, AsyncPipe],
   templateUrl: './student-details.component.html',
   styleUrl: './student-details.component.css',
 })
-export class StudentDetailsComponent implements OnInit, OnDestroy {
+export class StudentDetailsComponent {
   @Input() id!: string; // read id path param
   // don't forget: withComponentInputBinding in app.config
-  student: Student | undefined;
-  sub!: Subscription; // store sbuscription reference to manually unsubscribe in onDestroy
 
-  constructor(private studentsService: StudentsService) {}
+  student$ = this.studentsService
+    .getStudents()
+    .pipe(
+      map((data) => data.find((student) => student.id === Number(this.id))),
+    );
 
-  ngOnInit(): void {
-    this.sub = this.studentsService.getStudents().subscribe((data) => {
-      // console.log(data);
-      this.student = data.find((student) => student.id === Number(this.id));
-      // akid fi tari2a a7san mn keda bs ba3den ba2a
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+  constructor(private studentsService: StudentsService) {
+    console.log(this.student$);
   }
 }
--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -3,31 +3,18 @@ import { StudentsService } from '../../services/students.service';
 import { Student } from '../../shared/interfaces/student.interface';
 import { Subscription } from 'rxjs';
 import { StudentTableComponent } from '../../components/student-table/student-table.component';
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [StudentTableComponent, NgIf],
+  imports: [StudentTableComponent, NgIf, AsyncPipe],
   templateUrl: './students.component.html',
   styleUrl: './students.component.css',
 })
-export class StudentsComponent implements OnInit, OnDestroy {
-  students: Student[] = [];
-  sub!: Subscription; // store sbuscription reference to manually unsubscribe in onDestroy
-
+export class StudentsComponent {
+  students$ = this.studentsService.getStudents(); // store observable instead of data
   constructor(private studentsService: StudentsService) {}
-
-  ngOnInit(): void {
-    this.sub = this.studentsService.getStudents().subscribe((data) => {
-      // console.log(data);
-      this.students = data;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   onDeleteReceived(id: number) {
     // console.log('delete received', id);

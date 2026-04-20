80586d437f0eb81fe2431f3330921ea2b9a568c6
verinak <verinamichelk@gmail.com>
2026-04-20 02:01:49 +0200

    feat: implement student search by name

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StudentsService } from '../../services/students.service';
 import { Student } from '../../shared/interfaces/student.interface';
-import { Subscription } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  filter,
+  map,
+  Observable,
+  Subscription,
+} from 'rxjs';
 import { StudentTableComponent } from '../../components/student-table/student-table.component';
 import { AsyncPipe, NgIf } from '@angular/common';
 
@@ -13,11 +20,43 @@ import { AsyncPipe, NgIf } from '@angular/common';
   styleUrl: './students.component.css',
 })
 export class StudentsComponent {
-  students$ = this.studentsService.getStudents(); // store observable instead of data
+  // students$ = this.studentsService.getStudents(); // store observable instead of data
+  // filteredStudents$ = this.students$.pipe(
+  //   map((data) =>
+  //     data.filter((student) =>
+  //       `${student.firstName.toLowerCase()} ${student.lastName.toLowerCase()}`.includes(
+  //         this.searchQuery.toLowerCase(),
+  //       ),
+  //     ),
+  //   ),
+  // );
+
+  // https://medium.com/@bananicabananica/search-filter-with-rxjs-81d38863ecb2
+  searchQuery$ = new BehaviorSubject<string>('');
+
+  students$ = combineLatest([
+    this.studentsService.getStudents(),
+    this.searchQuery$,
+  ]).pipe(
+    map(([data, query]) => {
+      if (!query) return data;
+      return data.filter((student) =>
+        `${student.firstName} ${student.lastName}`
+          .toLowerCase()
+          .includes(query.toLowerCase()),
+      );
+    }),
+  );
+
   constructor(private studentsService: StudentsService) {}
 
   onDeleteReceived(id: number) {
     // console.log('delete received', id);
     this.studentsService.deleteStudent(id);
+  }
+
+  onSearchInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.searchQuery$.next(input.value);
   }
 }
